d0290a35383d8777654e4b20d87b057e5ba54743
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-07-30 19:48:55 +0200

    feat: add WASD support for Sharkie movement

--- js/game.js
+++ js/game.js
@@ -16,6 +16,10 @@ window.addEventListener("keydown", (e) => {
     if (e.keyCode == 38) keyboard.UP = true;     // ↑
     if (e.keyCode == 40) keyboard.DOWN = true;   // ↓
     if (e.keyCode == 32) keyboard.SPACE = true;  // Leertaste
+    if (e.keyCode == 87) keyboard.UP = true;    // W
+    if (e.keyCode == 65) keyboard.LEFT = true;  // A
+    if (e.keyCode == 83) keyboard.DOWN = true;  // S
+    if (e.keyCode == 68) keyboard.RIGHT = true; // D
 });
 
 window.addEventListener("keyup", (e) => {
@@ -24,4 +28,8 @@ window.addEventListener("keyup", (e) => {
     if (e.keyCode == 38) keyboard.UP = false;
     if (e.keyCode == 40) keyboard.DOWN = false;
     if (e.keyCode == 32) keyboard.SPACE = false;
+    if (e.keyCode == 87) keyboard.UP = false;
+    if (e.keyCode == 65) keyboard.LEFT = false;
+    if (e.keyCode == 83) keyboard.DOWN = false;
+    if (e.keyCode == 68) keyboard.RIGHT = false;
 });
