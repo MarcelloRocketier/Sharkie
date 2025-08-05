17223a24625149cdfc46276b960c7df78bd1ae32
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-08-05 16:52:03 +0200

    refactor: cleanup in movable object logic

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,44 +1,137 @@
-class MovableObject {
-    x = 120;
-    y = 180;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
-    speed = 0.15; // Default speed for movement
-    otherDirection = false;
+/**
+ * Basisklasse für alle beweglichen Objekte im Spiel
+ * Auch Hintergrundelemente sind movable
+ */
+class MovableObject extends DrawableObject {
+    speed = 0.15;
+    energy;
+    lastHit = 0;
+    offset = { x: 0, y: 0, width: 0, height: 0 };
+    checkAlreadyRunning = false;
+    animationStarted = false;
+    animationFinished = false;
+    waypointReached = false;
+    stopMovement = false;
 
-    // loadImage('img/test.png');
-    loadImage(path) {
-        this.img = new Image(); // this.img = document.getElementById('image') <img id="image">;
-        this.img.src = path;
+    /**
+     * Objekt in eine Richtung bewegen (horizontal / vertikal)
+     */
+    move(direction, startPoint, endPoint, speed) {
+        setInterval(() => {
+            if (!this.stopMovement) {
+                if (direction === 'horizontal') {
+                    if (this.x > endPoint) {
+                        this.waypointReached = true;
+                        this.imgMirrored = false;
+                    } else if (this.x < startPoint) {
+                        this.waypointReached = false;
+                        this.imgMirrored = true;
+                    }
+                    this.x += this.waypointReached ? -speed : speed;
+                } else if (direction === 'vertical') {
+                    if (this.y > endPoint) {
+                        this.waypointReached = true;
+                    } else if (this.y < startPoint) {
+                        this.waypointReached = false;
+                    }
+                    this.y += this.waypointReached ? -speed : speed;
+                }
+            }
+        }, 1000 / 60);
     }
 
     /**
-     * Loads an image from the given source path.
-     * @param {string} arr - The source path of the image to load.
+     * Spielanimation abspielen
+     * @param {string[]} images 
+     * @param {number} loop 0 = einmal, 1 = unendlich
      */
-    loadImages(arr) {
-        arr.forEach((path) => {
-        let img = new Image();
-        img.src = path;
-        this.imageCache[path] = img;
-    });
+    playAnimation(images, loop) {
+        if (loop === 0 && !this.animationFinished) {
+            if (!this.animationStarted) this.currentImage = 0;
+            this.animationStarted = true;
+
+            const i = this.currentImage % images.length;
+            this.img = this.imageCache[images[i]];
+            this.currentImage++;
+
+            if (this.currentImage === images.length) {
+                this.animationFinished = true;
+                this.animationStarted = false;
+            }
+        } else if (loop === 1) {
+            const i = this.currentImage % images.length;
+            this.img = this.imageCache[images[i]];
+            this.currentImage++;
+            this.animationFinished = false;
+        }
     }
 
-    moveRight() {
-        console.log('Moving right');        
+    /**
+     * Prüft, ob zwei Objekte sich überschneiden (Kollision)
+     */
+    isColliding(mo) {
+        return this.x + this.width - this.offset.width > mo.x + mo.offset.x &&
+               this.y + this.height - this.offset.height > mo.y + mo.offset.y &&
+               this.x + this.offset.x < mo.x + mo.width - mo.offset.width &&
+               this.y + this.offset.y < mo.y + mo.height - mo.offset.height;
     }
 
-    moveLeft() {
-    this.x -= this.speed;
-}
+    isCollidingX(mo) {
+        if (this.y + this.height - this.offset.height - 3 > mo.y + mo.offset.y &&
+            this.y + this.offset.y < mo.y + mo.height - mo.offset.height - 3) {
+            return this.x + this.width - this.offset.width > mo.x + mo.offset.x &&
+                   this.x + this.offset.x < mo.x + mo.width - mo.offset.width;
+        }
+    }
 
-    playAnimation(images) {
-    let i = this.currentImage % images.length;
-    let path = images[i];
-    this.img = this.imageCache[path];
-    this.currentImage++;
-}
+    isCollidingY(mo) {
+        if (this.x + this.width - this.offset.width - 3 > mo.x + mo.offset.x &&
+            this.x + this.offset.x < mo.x + mo.width - mo.offset.width - 3) {
+            return this.y + this.height - this.offset.height > mo.y + mo.offset.y &&
+                   this.y + this.offset.y < mo.y + mo.height - mo.offset.height;
+        }
+    }
+
+    /**
+     * Schaden erhalten durch gegnerischen Angriff
+     */
+    hit(damage) {
+        this.energy -= damage;
+        if (this.energy < 0) this.energy = 0;
+        else this.lastHit = new Date().getTime();
+    }
+
+    /**
+     * Gegner nach dem Tod wegschweben lassen (Pufferfisch)
+     */
+    floatAway(toRight) {
+        setInterval(() => {
+            this.x += toRight ? this.speed : -this.speed;
+            this.y -= this.speed;
+        }, 1000 / 60);
+    }
+
+    /**
+     * Gegner nach oben schweben lassen (Quallen)
+     */
+    floatAwayUp() {
+        setInterval(() => {
+            this.y -= this.speed;
+        }, 1000 / 60);
+    }
+
+    /**
+     * True, wenn Charakter kürzlich getroffen wurde
+     */
+    isHurt() {
+        const timePassed = (new Date().getTime() - this.lastHit) / 1000;
+        return timePassed < 1;
+    }
+
+    /**
+     * True, wenn Energie leer ist
+     */
+    isDead() {
+        return this.energy === 0;
+    }
 }
