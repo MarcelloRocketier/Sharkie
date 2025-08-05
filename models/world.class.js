85497ef3d7d3a4bbaebbbbbfa9c51362a7885d9b
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-08-05 17:05:25 +0200

    refactor: adjust world structure and simplify collision logic

--- models/world.class.js
+++ models/world.class.js
@@ -1,7 +1,5 @@
 /**
- * Spielwelt-Klasse
- * Hier werden alle Objekte des Spiels erstellt (Hintergründe, Charakter, Gegner, ...)
- * Zugriff auf Objekte über Konsole: world.<objekt>.<eigenschaft()/methode()>
+ * The entire game world – manages all rendered objects.
  */
 class World {
     canvas;
@@ -14,7 +12,6 @@ class World {
     character = new Character();
     levelDesignHelper = new LevelDesignHelper();
     level = levels[currentLevel];
-
     statusBarLife = new StatusBar('life', 'green', 100, 20, 0);
     statusBarCoins = new StatusBar('coins', 'green', 0, 20, 40);
     statusBarPoison = new StatusBar('poison', 'green', 0, 20, 80);
@@ -25,16 +22,12 @@ class World {
         this.keyboard = keyboard;
         this.ctx = canvas.getContext('2d');
 
-        this.setWorld();
         this.draw();
+        this.setWorld();
         this.checkCollisions();
-
-        if (mobileAndTabletCheck()) {
-            this.setupMobileUI();
-        }
-
-        this.handleFullscreenCanvas();
-        this.handleThemeSound();
+        this.setupMobileView();
+        this.observeFullscreen();
+        this.handleBackgroundSound();
     }
 
     setWorld() {
@@ -69,7 +62,6 @@ class World {
         this.addToWorld(this.statusBarLife);
         this.addToWorld(this.statusBarCoins);
         this.addToWorld(this.statusBarPoison);
-
         if (this.level.getEndBoss().endBossIntroduced) {
             this.addToWorld(this.statusBarEndBoss);
         }
@@ -80,71 +72,93 @@ class World {
         requestAnimationFrame(() => this.draw());
     }
 
-    addToWorld(obj) {
-        if (obj.imgMirrored) this.flipImage(obj);
-        obj.draw(this.ctx);
-        if (debugMode) obj.drawCollisionDetectionFrame(this.ctx);
-        if (obj.imgMirrored) this.undoFlipImage(obj);
+    addToWorld(movableObject) {
+        if (movableObject.imgMirrored) {
+            this.flipImage(movableObject);
+        }
+
+        movableObject.draw(this.ctx);
+
+        if (debugMode) {
+            movableObject.drawCollisionDetectionFrame(this.ctx);
+        }
+
+        if (movableObject.imgMirrored) {
+            this.undoFlipImage(movableObject);
+        }
     }
 
-    addObjectsToWorld(arr) {
-        arr.forEach(obj => this.addToWorld(obj));
+    addObjectsToWorld(objects) {
+        objects.forEach(obj => this.addToWorld(obj));
     }
 
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
-    flipImage(obj) {
+    flipImage(movableObject) {
         this.ctx.save();
-        this.ctx.translate(obj.width, 0);
+        this.ctx.translate(movableObject.width, 0);
         this.ctx.scale(-1, 1);
-        obj.x = obj.x * -1;
+        movableObject.x *= -1;
     }
 
-    undoFlipImage(obj) {
-        obj.x = obj.x * -1;
+    undoFlipImage(movableObject) {
+        movableObject.x *= -1;
         this.ctx.restore();
     }
 
-    setupMobileUI() {
+    setupMobileView() {
+        if (!mobileAndTabletCheck()) return;
+
         const hide = id => document.getElementById(id)?.classList.add('d-none');
         const show = id => document.getElementById(id)?.classList.remove('d-none');
 
-        ['game-title', 'canvas-frame-img', 'img-attribution'].forEach(hide);
-        ['mobile-fullscreen-btn', 'mobile-mute-btn', 'mobile-close-btn', 'mobile-ctrl-left', 'mobile-ctrl-right'].forEach(show);
+        hide('game-title');
+        hide('canvas-frame-img');
+        hide('img-attribution');
+
+        show('mobile-fullscreen-btn');
+        show('mobile-mute-btn');
+        show('mobile-close-btn');
+        show('mobile-ctrl-left');
+        show('mobile-ctrl-right');
 
         document.getElementById('canvas-wrapper')?.classList.add('fullscreen');
         document.getElementById('fullscreen-container')?.classList.add('fullscreen');
-        document.getElementById('canvas').style = 'width: 100%; height: 100%';
+        const canvas = document.getElementById('canvas');
+        if (canvas) canvas.style = 'width: 100%; height: 100%';
 
         toggleFullscreen();
     }
 
-    handleFullscreenCanvas() {
+    observeFullscreen() {
         setInterval(() => {
             const canvas = document.getElementById('canvas');
-            const fullMsg = document.getElementById('fullscreen-message');
-            const landscapeMsg = document.getElementById('landscape-message');
+            const fullscreenMessage = document.getElementById('fullscreen-message');
+            const landscapeMessage = document.getElementById('landscape-message');
 
-            if (fullscreen && canvas) canvas.classList.add('fullscreen');
-            else if (canvas) canvas.classList.remove('fullscreen');
+            if (fullscreen && canvas) {
+                canvas.classList.add('fullscreen');
+            } else if (!fullscreen && canvas) {
+                canvas.classList.remove('fullscreen');
+            }
 
             if (!mobileAndTabletCheck() && window.innerWidth <= 992) {
-                fullMsg?.classList.remove('d-none');
-                if (landscapeMsg) landscapeMsg.style = "opacity: 0";
+                fullscreenMessage?.classList.remove('d-none');
+                if (landscapeMessage) landscapeMessage.style = 'opacity: 0';
             } else {
-                fullMsg?.classList.add('d-none');
-                if (landscapeMsg) landscapeMsg.style = "opacity: 1";
+                fullscreenMessage?.classList.add('d-none');
+                if (landscapeMessage) landscapeMessage.style = 'opacity: 1';
             }
         }, 1000 / 60);
     }
 
-    handleThemeSound() {
+    handleBackgroundSound() {
         setInterval(() => {
             if (soundOn && !this.level.getEndBoss().endBossAlreadyTriggered && !levelEnded) {
                 this.MAIN_SOUND.play();
-                this.MAIN_SOUND.addEventListener('ended', function () {
+                this.MAIN_SOUND.addEventListener('ended', function() {
                     this.currentTime = 0;
                     this.play();
                 }, false);
@@ -157,87 +171,86 @@ class World {
 
     checkCollisions() {
         setInterval(() => {
-            this.handleEnemyCollisions();
-            this.handleBubbleCollisions();
-            this.handleEndBossCollisions();
-            this.handleCollectableCollisions();
+            this.checkEnemyCollisions();
+            this.checkCoinCollisions();
+            this.checkLifeCollisions();
+            this.checkPoisonCollisions();
         }, 200);
     }
 
-    handleEnemyCollisions() {
+    checkEnemyCollisions() {
         this.level.enemies.forEach(enemy => {
             if (this.character.isColliding(enemy) && !enemy.isDead() && !this.character.isFinSlapping) {
                 this.character.hit(enemy.attack);
-                this.statusBarLife.setPercentage(this.character.energy, 'life', 'green');
-                this.character.hitBy = enemy.constructor.name;
-                if (enemy instanceof EndBoss) this.level.getEndBoss().isCollidingWithCharacter = true;
-                if (debugLogStatements) console.log('Colliding with:', enemy);
+                this.statusBarLife.setPercentage(this.character.energy, this.statusBarLife.type, this.statusBarLife.color);
+                if (enemy instanceof PufferFish) this.character.hitBy = 'PufferFish';
+                if (enemy instanceof JellyFishRegular || enemy instanceof JellyFishDangerous) this.character.hitBy = 'JellyFish';
+                if (enemy instanceof EndBoss) {
+                    this.character.hitBy = 'EndBoss';
+                    this.level.getEndBoss().isCollidingWithCharacter = true;
+                }
             }
 
             if (this.character.isColliding(enemy) && this.character.isFinSlapping && enemy instanceof PufferFish) {
                 enemy.hit(this.character.attack);
                 enemy.stopMovement = true;
                 enemy.floatAway(this.character.imgMirrored);
-                if (debugLogStatements) console.log('Fin slap:', enemy);
             }
-        });
-    }
 
-    handleBubbleCollisions() {
-        this.level.enemies.forEach(enemy => {
-            if (this.bubble instanceof Bubble && this.bubble.isColliding(enemy) && (enemy instanceof JellyFishRegular || enemy instanceof JellyFishDangerous)) {
-                enemy.hit(this.bubble.attack);
-                enemy.stopMovement = true;
-                enemy.speed = 1;
-                enemy.floatAwayUp();
-                this.bubble = undefined;
-            }
-        });
-    }
-
-    handleEndBossCollisions() {
-        this.level.enemies.forEach(enemy => {
             if (this.character.isColliding(enemy) && this.character.isFinSlapping && enemy instanceof EndBoss) {
                 enemy.hit(this.character.attack);
-                this.statusBarEndBoss.setPercentage((enemy.energy / 200) * 100, 'life', 'orange');
+                this.statusBarEndBoss.setPercentage((enemy.energy / 200) * 100, this.statusBarEndBoss.type, this.statusBarEndBoss.color);
             }
 
-            if (this.bubble && this.bubble.isColliding(enemy) && enemy instanceof EndBoss) {
-                enemy.hit(this.bubble.attack);
-                this.statusBarEndBoss.setPercentage((enemy.energy / 200) * 100, 'life', 'orange');
-                this.bubble = undefined;
+            if (this.bubble && this.bubble.isColliding(enemy)) {
+                if (enemy instanceof JellyFishRegular || enemy instanceof JellyFishDangerous) {
+                    enemy.hit(this.bubble.attack);
+                    enemy.stopMovement = true;
+                    enemy.speed = 1;
+                    enemy.floatAwayUp();
+                    this.bubble = undefined;
+                }
+                if (enemy instanceof EndBoss) {
+                    enemy.hit(this.bubble.attack);
+                    this.statusBarEndBoss.setPercentage((enemy.energy / 200) * 100, this.statusBarEndBoss.type, this.statusBarEndBoss.color);
+                    this.bubble = undefined;
+                }
             }
         });
     }
 
-    handleCollectableCollisions() {
+    checkCoinCollisions() {
         this.level.coins.forEach(coin => {
             if (this.character.isColliding(coin)) {
-                const index = this.level.coins.indexOf(coin);
-                const total = this.level.coins.length + this.character.coins;
+                let index = this.level.coins.indexOf(coin);
+                let total = this.level.coins.length + this.character.coins;
                 this.character.coins++;
-                this.statusBarCoins.setPercentage((this.character.coins / total) * 100, 'coins', 'green');
+                this.statusBarCoins.setPercentage((this.character.coins / total) * 100, this.statusBarCoins.type, this.statusBarCoins.color);
                 this.level.coins.splice(index, 1);
             }
         });
+    }
 
+    checkLifeCollisions() {
         this.level.life.forEach(life => {
             if (this.character.isColliding(life)) {
-                const index = this.level.life.indexOf(life);
-                this.character.energy = Math.min(this.character.energy + (this.character.energy > 90 ? 5 : 10), 100);
-                this.statusBarLife.setPercentage(this.character.energy, 'life', 'green');
+                let index = this.level.life.indexOf(life);
+                this.character.energy = Math.min(100, this.character.energy + (this.character.energy < 90 ? 10 : 5));
+                this.statusBarLife.setPercentage(this.character.energy, this.statusBarLife.type, this.statusBarLife.color);
                 this.level.life.splice(index, 1);
             }
         });
+    }
 
+    checkPoisonCollisions() {
         this.level.poison.forEach(poison => {
             if (this.character.isColliding(poison)) {
-                const index = this.level.poison.indexOf(poison);
+                let index = this.level.poison.indexOf(poison);
                 this.level.totalPoison = this.level.poison.length + this.level.collectedPoison;
                 this.character.poison++;
-                this.statusBarPoison.setPercentage((this.character.poison / this.level.totalPoison) * 100, 'poison', 'green');
-                this.level.poison.splice(index, 1);
                 this.level.collectedPoison++;
+                this.statusBarPoison.setPercentage((this.character.poison / this.level.totalPoison) * 100, this.statusBarPoison.type, this.statusBarPoison.color);
+                this.level.poison.splice(index, 1);
             }
         });
     }
