0ff5b702dbc49d2251b60be70e20b8f4d6656f39
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-08-05 17:02:27 +0200

    feat: implement custom Level class structure with dynamic object references

--- models/level.class.js
+++ models/level.class.js
@@ -1,20 +1,43 @@
+/**
+ * Stores all level-specific objects
+ * Each level has different backgrounds, enemies and items
+ */
 class Level {
-    enemies;
-    character;
-    bubbles;
     backgroundObjects;
-    coins;
-    poisons;
-    obstacles;
-    poisonObjects;
-    level_end_x = 5000; 
+    barriers;
+    enemies;
+    coins = 0;
+    life = 0;
+    poison = 0;
+    totalPoison = 0;
+    collectedPoison = 0;
+    level_end_x;
 
-    constructor(enemies, bubbles, backgroundObjects, coins, poisons, obstacles = []) {
-        this.enemies = enemies;
-        this.bubbles = bubbles;
+    /**
+     * Initializes all level objects
+     * @param {Array} backgroundObjects 
+     * @param {Array} coins 
+     * @param {Array} life 
+     * @param {Array} poison 
+     * @param {Array} enemies 
+     * @param {Array} barriers 
+     * @param {number} level_end_x 
+     */
+    constructor(backgroundObjects, coins, life, poison, enemies, barriers, level_end_x) {
         this.backgroundObjects = backgroundObjects;
         this.coins = coins;
-        this.poisons = poisons;
-        this.obstacles = obstacles;
+        this.life = life;
+        this.poison = poison;
+        this.enemies = enemies;
+        this.barriers = barriers;
+        this.level_end_x = level_end_x;
+        this.getEndBoss(); // Cache access to EndBoss if present
+    }
+
+    /**
+     * Returns the EndBoss object if available
+     */
+    getEndBoss() {
+        return this.enemies.find(e => e instanceof EndBoss);
     }
 }
