ea56cef1b4239fe47b12f7adfcf73a3eed398f54
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-08-05 19:55:58 +0200

    style: reword bubble class comments and clarify float behavior

--- js/models/bubble.class.js
+++ js/models/bubble.class.js
@@ -5,7 +5,7 @@ class Bubble extends MovableObject {
     attack = 10;
 
     constructor(startX, startY, otherDirection) {
-        super().loadImage('assets/img/1._Sharkie/4._Attack/Bubble_Trap/Bubble.png');
+        super().loadImage('./assets/img/1._Sharkie/4._Attack/Bubble_Trap/Bubble.png');
         this.x = startX;
         this.y = startY;
         this.otherDirection = otherDirection;
@@ -13,16 +13,23 @@ class Bubble extends MovableObject {
     }
 
     /**
-     * Moves the bubble diagonally upward depending on the direction
+     * Causes the bubble to float in a diagonal direction
      */
     float() {
+        // Adjust start position if bubble is coming from the right side
         if (this.otherDirection) {
-            this.x -= 200; // Shift bubble to start from character's correct side
+            this.x -= 200;
         }
-
         setInterval(() => {
-            this.x += this.otherDirection ? -this.speed : this.speed;
-            this.y -= this.speed;
+            if (this.otherDirection) {
+                // Bubble moves diagonally up-left
+                this.x -= this.speed;
+                this.y -= this.speed;
+            } else {
+                // Bubble moves diagonally up-right
+                this.x += this.speed;
+                this.y -= this.speed;
+            }
         }, 1000 / 60);
     }
-} 
+}
