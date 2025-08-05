b709fff084d3fdbb4310c9ec07141c7eaab7c6e7
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-08-05 19:54:31 +0200

    refactor: personalize background object comments and formatting

--- js/models/background-object.class.js
+++ js/models/background-object.class.js
@@ -1,11 +1,22 @@
+/**
+ * Represents background elements for each level section
+ */
 class BackgroundObject extends MovableObject {
-
     width = 720;
     height = 480;
-   
-    constructor(imagePath, x, y) {
-        super().loadImage(imagePath);
-        this.x = x; 
-        this.y = 480 - this.height; 
+
+    constructor(scene, section, index, levelSection) {
+        // Load the appropriate background image based on parameters
+        super().loadImage(BACKGROUND_IMAGES.IMAGES[scene][section][index]);
+
+        // Position horizontally according to the section index
+        if (levelSection === 0) {
+            this.x = 0;
+        } else {
+            this.x = 719 * levelSection;
+        }
+
+        // Align vertically to the bottom of the canvas
+        this.y = 480 - this.height;
     }
 }
