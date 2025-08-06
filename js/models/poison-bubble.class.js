e1adb4f11a935f9bfce6af1291e95e9c635e07e8
Marcel Langenhorst <marcelrupp95@gmail.com>
2025-08-06 13:06:56 +0200

    style: refine poison bubble class comments for clarity

--- js/models/poison-bubble.class.js
+++ js/models/poison-bubble.class.js
@@ -1,33 +1,29 @@
-/**
- * Represents a floating poison bubble used for attack
- */
 class PoisonBubble extends MovableObject {
     width = 60;
     height = 60;
     speed = 1.02;
     attack = 30;
 
-    constructor(startX, startY, flippedDirection) {
+    constructor(startX, startY, otherDirection) {
         super().loadImage('./assets/img/1._Sharkie/4._Attack/Bubble_Trap/Poisoned_Bubble_(For_Whale).png');
         this.x = startX;
         this.y = startY;
-        this.otherDirection = flippedDirection;
-        this.startFloating();
+        this.otherDirection = otherDirection;
+        this.float();
     }
 
     /**
-     * Starts the floating animation of the poison bubble
+     * Makes the bubble float in a diagonal direction based on orientation
      */
-    startFloating() {
+    float() {
         if (this.otherDirection) {
-            this.x -= 200; // Adjust position if character is facing left
+            this.x -= 200; // Adjust bubble start position when character faces right
         }
-
         setInterval(() => {
-            if (this.otherDirection) {
+            if (this.otherDirection) { // Moves bubble diagonally up-left
                 this.x -= this.speed;
                 this.y -= this.speed;
-            } else {
+            } else { // Moves bubble diagonally up-right
                 this.x += this.speed;
                 this.y -= this.speed;
             }
